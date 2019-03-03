aeaa87214c2eced2d795555fdaaaa256a06d2659
Will Huang <will_huang@trend.com.tw>
2019-03-03 22:56:52 +0800

    Add more concurrency

--- src/main/java/com/will/rabbitmqconsumer/consumer/FixedRateConsumer.java
+++ src/main/java/com/will/rabbitmqconsumer/consumer/FixedRateConsumer.java
@@ -3,12 +3,15 @@ package com.will.rabbitmqconsumer.consumer;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class FixedRateConsumer {
 
-	@RabbitListener(queues = "course.fixedrate")
-	public void listen(String message) {
-		System.out.println("Consuming :" + message);
+	@RabbitListener(queues = "course.fixedrate", concurrency = "4")
+	public void listen(String message) throws InterruptedException{
+		Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
+		System.out.println(Thread.currentThread().getName() + " Consuming :" + message);
 	}
 
 }
