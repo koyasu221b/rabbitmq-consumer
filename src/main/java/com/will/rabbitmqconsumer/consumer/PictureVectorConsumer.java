1b5088b91610197754d7d827eec6ffd91f0b7560
Will Huang <will_huang@trend.com.tw>
2019-03-18 22:56:37 +0800

    Add direct practice

---
+++ src/main/java/com/will/rabbitmqconsumer/consumer/PictureVectorConsumer.java
@@ -0,0 +1,22 @@
+package com.will.rabbitmqconsumer.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.rabbitmqconsumer.entity.Picture;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class PictureVectorConsumer {
+
+	private ObjectMapper objectMapper = new ObjectMapper();
+
+	@RabbitListener(queues = "q.picture.vector")
+	public void listen(String message) throws IOException {
+		Picture p = objectMapper.readValue(message, Picture.class);
+		System.out.println("Converting to image & creating thumbnail & publishing :" + p);
+
+	}
+}
